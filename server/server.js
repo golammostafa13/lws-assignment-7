6dd4224ac068178af506bb088b024245fd669d64
Golam Mostafa <mostafarmstu@gmail.com>
2022-09-06 04:51:42 +0600

    update

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const data = require("./db_transactions.json");
-
+const cors = require(cors);
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router(data);
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 9000;
 
+server.use(cors());
 server.use(middlewares);
 server.use(router);
 
